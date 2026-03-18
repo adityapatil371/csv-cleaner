cf3b9735c650b73bf0f9f8d1c732d56c520f2927
adityapatil371 <adityapatil371@gmail.com>
2026-03-18 21:46:50 +0530

    added load, report and save methods to DataCleaner

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -13,12 +13,44 @@ class DataCleaner:
         self.count = 0
         self.data = []
 
+    def load(self) -> None:
+        try:
+            with open(self.input) as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    self.data.append(row)
+                    self.count += 1
+        except FileNotFoundError:
+            print("incorect path")
+            sys.exit(1)
+        except UnicodeDecodeError:
+            print("incorect file type")
+            sys.exit(1)
+    
+    def report(self) -> None:
+        if not self.data:
+            print("initialise load first")
+            sys.exit(1)
+        print("summary: ")
+        print(f"column names are {self.data[0].keys()}")
+        print(f"row count is {self.count}")
+        print(f"file size is {Path(self.input).stat().st_size}")
 
+    def save(self) -> None:
+        if not self.data:
+            print("initialise load first")
+            sys.exit(1)
+        with open(self.output, 'w') as file:
+            writer = csv.DictWriter(file, fieldnames= self.data[0].keys())
+            writer.writeheader()
+            writer.writerows(self.data)
 
 def main() -> None:
     args = user_input()
     cleaner = DataCleaner(args)
-
+    cleaner.load()
+    cleaner.report()
+    cleaner.save()
 
 def user_input() -> argparse.Namespace:
     parser =argparse.ArgumentParser()
@@ -28,7 +60,6 @@ def user_input() -> argparse.Namespace:
     parser.add_argument("--drop-duplicates", help="add if true", action='store_true')
     args = parser.parse_args()
     return args
-
 
 if __name__ == "__main__":
     main()
